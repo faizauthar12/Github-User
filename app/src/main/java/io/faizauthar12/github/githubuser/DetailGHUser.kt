9ba929ade5eb65ffca379469ebf943fc25bbb72b
Faiz Authar <faizauthar@gmail.com>
2020-10-20 21:19:55 +0700

    GHUser: Detail screen barebone layout

---
+++ app/src/main/java/io/faizauthar12/github/githubuser/DetailGHUser.kt
@@ -0,0 +1,11 @@
+package io.faizauthar12.github.githubuser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailGHUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_g_h_user)
+    }
+}
