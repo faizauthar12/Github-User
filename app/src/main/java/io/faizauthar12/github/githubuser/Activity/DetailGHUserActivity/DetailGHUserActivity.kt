6de788846071b43ca1b63a121bc9115182cab9dc
Faiz Authar <faizauthar@gmail.com>
2020-11-10 10:27:17 +0700

    GHUser: major cleanup for API implementation

--- app/src/main/java/io/faizauthar12/github/githubuser/Activity/DetailGHUserActivity/DetailGHUserActivity.kt
+++ app/src/main/java/io/faizauthar12/github/githubuser/Activity/DetailGHUserActivity/DetailGHUserActivity.kt
@@ -2,39 +2,11 @@ package io.faizauthar12.github.githubuser.Activity.DetailGHUserActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import io.faizauthar12.github.githubuser.Model.GHUser
 import io.faizauthar12.github.githubuser.R
-import kotlinx.android.synthetic.main.activity_detail_g_h_user.*
 
 class DetailGHUserActivity : AppCompatActivity() {
-    companion object{
-        const val EXTRA_GHUSER = "extra_ghuser"
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_g_h_user)
-
-        val ghuser = intent.getParcelableExtra<GHUser>(EXTRA_GHUSER) as GHUser
-
-        TV_GHUser_name.text = ghuser.name
-        TV_GHUser_username.text = getString(R.string.GHU_username, ghuser.username)
-        TV_GHUser_Followers.text = getString(R.string.GHU_followers, ghuser.followers)
-        TV_GHUser_Following.text = getString(R.string.GHU_following, ghuser.following)
-        GHUser_repository.text = getString(R.string.GHU_repository, ghuser.repository)
-        GHUser_location.text = ghuser.location
-        GHUser_company.text = ghuser.company
-
-        Glide.with(applicationContext)
-                .load(ghuser.avatar)
-                .apply(RequestOptions().override(100,100))
-                .into(IV_GHUser_Avatar)
-
-        setActionBarTitle(ghuser.name)
-    }
-    private fun setActionBarTitle(title: String?) {
-        supportActionBar?.title = title
     }
 }
--- app/src/main/java/io/faizauthar12/github/githubuser/MainActivity.kt
+++ app/src/main/java/io/faizauthar12/github/githubuser/MainActivity.kt
@@ -1,64 +1,11 @@
 package io.faizauthar12.github.githubuser
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import io.faizauthar12.github.githubuser.Activity.DetailGHUserActivity.DetailGHUserActivity
-import io.faizauthar12.github.githubuser.Adapter.ListGHUserAdapter
-import io.faizauthar12.github.githubuser.Model.GHUser
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val list = ArrayList<GHUser>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        rv_GHUser.setHasFixedSize(true)
-        list.addAll(getListGHUsers())
-        showRecyclerList()
-    }
-
-    private fun getListGHUsers(): ArrayList<GHUser> {
-        val dataUsername = resources.getStringArray(R.array.username)
-        val dataName = resources.getStringArray(R.array.name)
-        val dataLocation = resources.getStringArray(R.array.location)
-        val dataRepository = resources.getStringArray(R.array.repository)
-        val dataCompany = resources.getStringArray(R.array.company)
-        val dataFollowers = resources.getStringArray(R.array.followers)
-        val dataFollowing = resources.getStringArray(R.array.following)
-        val dataAvatar = resources.obtainTypedArray(R.array.avatar)
-
-        val listGHUser = ArrayList<GHUser>()
-        for (position in dataUsername.indices) {
-            val ghUser = GHUser(
-                    dataUsername[position],
-                    dataName[position],
-                    dataLocation[position],
-                    dataRepository[position],
-                    dataCompany[position],
-                    dataFollowers[position],
-                    dataFollowing[position],
-                    dataAvatar.getResourceId(position,-1)
-            )
-            listGHUser.add(ghUser)
-        }
-        return listGHUser
-    }
-
-    private fun showRecyclerList() {
-        rv_GHUser.layoutManager = LinearLayoutManager(this)
-        val listGHUserAdapter = ListGHUserAdapter(list)
-        rv_GHUser.adapter = listGHUserAdapter
-
-        listGHUserAdapter.setOnItemClickCallback(object : ListGHUserAdapter.OnItemClickCallback {
-            override fun onItemClicked(data: GHUser) {
-                val intent = Intent(this@MainActivity, DetailGHUserActivity::class.java)
-                intent.putExtra(DetailGHUserActivity.EXTRA_GHUSER,data)
-                startActivity(intent)
-            }
-        })
     }
 }
