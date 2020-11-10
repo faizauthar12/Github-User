9135f9f8deb06783b2e126a55d72bbca0fb19f6d
Faiz Authar <faizauthar@gmail.com>
2020-11-10 11:24:00 +0700

    GHUser: API[4/4]: Implement API logic in main activity

--- app/src/main/java/io/faizauthar12/github/githubuser/MainActivity.kt
+++ app/src/main/java/io/faizauthar12/github/githubuser/MainActivity.kt
@@ -2,10 +2,54 @@ package io.faizauthar12.github.githubuser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.faizauthar12.github.githubuser.Adapter.UsernameAdapter
+import io.faizauthar12.github.githubuser.Model.Username
+import io.faizauthar12.github.githubuser.ViewModel.UsernameViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var usernameAdapter: UsernameAdapter
+    private lateinit var usernameViewModel: UsernameViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        usernameAdapter = UsernameAdapter()
+        usernameAdapter.notifyDataSetChanged()
+
+        rv_GHUser.layoutManager = LinearLayoutManager(this)
+        rv_GHUser.adapter = usernameAdapter
+
+        usernameViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(UsernameViewModel::class.java)
+
+        showLoading(true)
+
+        val username = "doraemon"
+        usernameViewModel.setUsername(username)
+
+        usernameViewModel.getUsername().observe(this, { GHUsername ->
+            if (GHUsername != null) {
+                usernameAdapter.setData(GHUsername)
+                showLoading(false)
+            }
+        })
+        usernameAdapter.setOnItemClickCallback(object : UsernameAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Username) {
+                Toast.makeText(this@MainActivity, "SELECTED", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            progressBar.visibility = View.VISIBLE
+        } else {
+            progressBar.visibility = View.GONE
+        }
     }
 }
