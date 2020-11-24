337d01ef26a8cd08fdcd25e0f28599510cc8066f
Faiz Authar <faizauthar@gmail.com>
2020-11-24 15:42:04 +0700

    GHUser: detailAPI[2/2]: Implement API to logic program

--- app/src/main/java/io/faizauthar12/github/githubuser/Activity/DetailGHUserActivity/DetailGHUserActivity.kt
+++ app/src/main/java/io/faizauthar12/github/githubuser/Activity/DetailGHUserActivity/DetailGHUserActivity.kt
@@ -2,8 +2,18 @@ package io.faizauthar12.github.githubuser.Activity.DetailGHUserActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import io.faizauthar12.github.githubuser.BuildConfig
 import io.faizauthar12.github.githubuser.Model.Username
 import io.faizauthar12.github.githubuser.R
+import kotlinx.android.synthetic.main.activity_detail_g_h_user.*
+import org.json.JSONObject
 
 class DetailGHUserActivity : AppCompatActivity() {
     companion object {
@@ -16,5 +26,76 @@ class DetailGHUserActivity : AppCompatActivity() {
 
         /* get data from main activity */
         val username = intent.getParcelableExtra<Username>(EXTRA_USER) as Username
+
+        /* hidden few iv */
+        imageController()
+
+        /* get detail of selected username */
+        getDetail(username.username)
+    }
+
+    private fun imageController() {
+        IV_Location.visibility = View.GONE
+        IV_Company.visibility = View.GONE
+    }
+
+    private fun getDetail(username: String?) {
+        val apiKey = BuildConfig.API_KEY
+        val apiUrl = "https://api.github.com"
+
+        val client = AsyncHttpClient()
+        val url = "$apiUrl/users/$username"
+
+        client.addHeader("Authorization", "token $apiKey")
+        client.addHeader("User-Agent", "request")
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray) {
+                try {
+                    /* Parsing json to layout */
+                    val result = String(responseBody)
+                    val responseObject = JSONObject(result)
+                    var name = responseObject.getString("name")
+                    val login = responseObject.getString("login")
+                    val location = responseObject.getString("location")
+                    val company = responseObject.getString("company")
+
+                    /* Get Avatar */
+                    Glide.with(applicationContext)
+                        .load(responseObject.getString("avatar_url"))
+                        .apply(RequestOptions().override(100,100))
+                        .into(IV_GHUser_Avatar)
+
+                    /* Set it to layout xml */
+                    TV_GHUser_username.text = "@${login}"
+                    if (name == "null"){
+                        TV_GHUser_name.visibility = View.GONE
+                    } else {
+                        TV_GHUser_name.text = name
+                    }
+
+                    if (location == "null"){
+                        TV_Location.visibility = View.GONE
+                    } else {
+                        TV_Location.text = location
+                        IV_Location.visibility = View.VISIBLE
+                    }
+
+                    if (company == "null"){
+                        TV_Company.visibility = View.GONE
+                    } else {
+                        TV_Company.text = company
+                        IV_Company.visibility = View.VISIBLE
+                    }
+
+                } catch (e: Exception) {
+                    Log.d("Exception", e.message.toString())
+                }
+
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable?) {
+                Log.d("onFailure", error?.message.toString())
+            }
+        })
     }
 }
