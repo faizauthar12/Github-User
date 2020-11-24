4b54762d7162eca20fca3aa2d8c78a6bfb26adc6
Faiz Authar <faizauthar@gmail.com>
2020-11-24 14:42:25 +0700

    GHUser: usernameAPI[5/5]: implement API in MainActivity

--- app/src/main/java/io/faizauthar12/github/githubuser/MainActivity.kt
+++ app/src/main/java/io/faizauthar12/github/githubuser/MainActivity.kt
@@ -1,11 +1,52 @@
+
 package io.faizauthar12.github.githubuser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.faizauthar12.github.githubuser.Adapter.UsernameAdapter
+import io.faizauthar12.github.githubuser.ViewModel.UsernameViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var usernameAdapter: UsernameAdapter
+    private lateinit var usernameViewModel: UsernameViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvResults("Doraemon")
+    }
+
+    private fun rvResults(username: String) {
+        usernameAdapter = UsernameAdapter()
+        usernameAdapter.notifyDataSetChanged()
+
+        rv_GHUser.layoutManager = LinearLayoutManager(this)
+        rv_GHUser.adapter = usernameAdapter
+
+        usernameViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(UsernameViewModel::class.java)
+
+        showLoading(true)
+
+        usernameViewModel.setUsername(username)
+
+        usernameViewModel.getUsername().observe(this, { GHUsername ->
+            if (GHUsername != null) {
+                usernameAdapter.setData(GHUsername)
+                showLoading(false)
+            }
+        })
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            progressBar.visibility = View.VISIBLE
+        } else {
+            progressBar.visibility = View.GONE
+        }
     }
 }
