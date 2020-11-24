337c5dfb75cbf1ac82380d34aea6970608b1a2b4
Faiz Authar <faizauthar@gmail.com>
2020-11-24 13:37:00 +0700

    GHUser: Implement search

--- app/src/main/java/io/faizauthar12/github/githubuser/MainActivity.kt
+++ app/src/main/java/io/faizauthar12/github/githubuser/MainActivity.kt
@@ -1,9 +1,12 @@
 package io.faizauthar12.github.githubuser
 
+import android.app.SearchManager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import io.faizauthar12.github.githubuser.Activity.DetailGHUserActivity.DetailGHUserActivity
@@ -19,9 +22,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val username = "doraemon"
+    }
 
-        rvResults(username)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+
+        val searchManager = getSystemService(SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            /*
+             * get username
+             */
+            override fun onQueryTextSubmit(query: String): Boolean {
+                rvResults(query)
+                return true
+            }
+
+            /*
+             * respons every character changes
+             */
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        return true
     }
 
     private fun rvResults(username: String) {
