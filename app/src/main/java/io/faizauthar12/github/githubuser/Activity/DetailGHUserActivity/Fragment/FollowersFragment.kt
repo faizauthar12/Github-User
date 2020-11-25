9ed53a7d704d073b7c3d52909332dd078358c5b3
Faiz Authar <faizauthar@gmail.com>
2020-11-25 12:21:45 +0700

    GHUser: FollowersAPI[5/5]: Implement API into program logic

--- app/src/main/java/io/faizauthar12/github/githubuser/Activity/DetailGHUserActivity/Fragment/FollowersFragment.kt
+++ app/src/main/java/io/faizauthar12/github/githubuser/Activity/DetailGHUserActivity/Fragment/FollowersFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.faizauthar12.github.githubuser.Adapter.DetailGHUserActivity.FollowersAdapter
 import io.faizauthar12.github.githubuser.R
+import io.faizauthar12.github.githubuser.ViewModel.FollowersViewModel
+import kotlinx.android.synthetic.main.fragment_followers.*
 
 class FollowersFragment : Fragment() {
+    private lateinit var followersAdapter : FollowersAdapter
+    private lateinit var followersViewModel : FollowersViewModel
+
     companion object {
         private val ARG_USERNAME = "username"
 
@@ -24,9 +32,37 @@ class FollowersFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_followers, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val username = arguments?.getString(FollowersFragment.ARG_USERNAME)
+
+        rvFollowers(username)
+    }
+
+    private fun rvFollowers(username: String?) {
+        followersAdapter = FollowersAdapter()
+        followersAdapter.notifyDataSetChanged()
+
+        rv_followers.layoutManager = LinearLayoutManager(activity)
+        rv_followers.adapter = followersAdapter
+
+        followersViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(FollowersViewModel::class.java)
+        if (username != null) {
+            followersViewModel.setFollowers(username)
+        }
+
+        followersViewModel.getFollowers().observe(viewLifecycleOwner, {FollowersItems ->
+            if (FollowersItems != null) {
+                followersAdapter.setData(FollowersItems)
+            }
+        })
     }
 }
