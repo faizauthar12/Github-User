a177a31c261e42624c13647281bd431988122dc3
Faiz Authar <faizauthar@gmail.com>
2020-11-25 11:47:44 +0700

    GHUser: FollowingAPI[5/5]: Implement API into program logic

--- app/src/main/java/io/faizauthar12/github/githubuser/Activity/DetailGHUserActivity/Fragment/FollowingFragment.kt
+++ app/src/main/java/io/faizauthar12/github/githubuser/Activity/DetailGHUserActivity/Fragment/FollowingFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.faizauthar12.github.githubuser.Adapter.DetailGHUserActivity.FollowingAdapter
 import io.faizauthar12.github.githubuser.R
+import io.faizauthar12.github.githubuser.ViewModel.FollowingViewModel
+import kotlinx.android.synthetic.main.fragment_following.*
 
 class FollowingFragment : Fragment() {
+    private lateinit var followingAdapter: FollowingAdapter
+    private lateinit var followingViewModel: FollowingViewModel
+
     companion object {
         private val ARG_USERNAME = "username"
 
@@ -25,10 +33,40 @@ class FollowingFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_following, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val username = arguments?.getString(ARG_USERNAME)
+
+        rvFollowing(username)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    private fun rvFollowing(username: String?) {
+        followingAdapter = FollowingAdapter()
+        followingAdapter.notifyDataSetChanged()
+
+        rv_following.layoutManager = LinearLayoutManager(activity)
+        rv_following.adapter = followingAdapter
+
+        followingViewModel = ViewModelProvider(this,ViewModelProvider.NewInstanceFactory()).get(FollowingViewModel::class.java)
+        if (username != null) {
+            followingViewModel.setFollowing(username)
+        }
+
+        followingViewModel.getFollowing().observe(viewLifecycleOwner, {FollowingItems ->
+            if (FollowingItems != null) {
+                followingAdapter.setData(FollowingItems)
+            }
+        })
     }
 }
